b2da27b56c0644ddadd456d882688e285bb375be
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-10-08 14:42:54 +0300

    Updates

---
+++ precache-manifest.8380677ab8f9a29bfb1ba3b5d72b514d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f08029e223c1d52e0c57e77594abb89",
+    "url": "/Speaking-Club-Questions/index.html"
+  },
+  {
+    "revision": "ecf077e62aa09461540e",
+    "url": "/Speaking-Club-Questions/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4460a70e4fad0592388c",
+    "url": "/Speaking-Club-Questions/static/js/2.591c2c1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Speaking-Club-Questions/static/js/2.591c2c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf077e62aa09461540e",
+    "url": "/Speaking-Club-Questions/static/js/main.d0ea5ab9.chunk.js"
+  },
+  {
+    "revision": "31e7c139c4f6fc7379b5",
+    "url": "/Speaking-Club-Questions/static/js/runtime-main.576c06c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Speaking-Club-Questions/static/media/logo.5d5d9eef.svg"
+  }
+]);
